835683f0ef9e6ed1b9015f7037d4a260e3c89f0f
Jefferson <40587602+jefftdb@users.noreply.github.com>
2024-06-14 15:50:22 -0300

    Adicionei js, e o modal

--- rifa/static/rifa/js/js.js
+++ rifa/static/rifa/js/js.js
@@ -1,3 +1,23 @@
 
 
+
+document.addEventListener('DOMContentLoaded', function () {
+  const myModal = document.getElementById('myModal')
+  myModal.addEventListener('shown.bs.modal', () => {
+
+    var button = event.relatedTarget; // Elemento que acionou o modal
+    var rifaId = button.getAttribute('data-rifa-id');
+    var numeroId = button.getAttribute('data-numero-id');
+
+    // Use rifaId e numeroId conforme necessário
+    console.log('Rifa ID:', rifaId);
+    console.log('Número ID:', numeroId);
+
+    // Adicionando a ação ao botão de "Save changes"
+    var saveButton = document.getElementById('saveChangesButton');
+    saveButton.onclick = function () {
+        window.location.href = `/rifa/edit_rifa/${rifaId}/${numeroId}`;
+    };
+  })
+});
   
